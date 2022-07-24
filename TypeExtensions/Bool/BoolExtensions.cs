b58714838328fa24f57fa98527ea143bf790cb41
Mike Obermiller <michaeljobermiller@gmail.com>
2022-07-23 23:24:26 -0400

    Bool add from yes no

--- TypeExtensions/Bool/BoolExtensions.cs
+++ TypeExtensions/Bool/BoolExtensions.cs
@@ -2,6 +2,10 @@
 
 public static class BoolExtensions
 {
+    public static bool FromYesNo(this string x) => x.ToLowerInvariant() == "yes";
+
+    public static bool FromYN(this string x) => x.ToLowerInvariant() == "y";
+    
     public static string ToYesNo(this bool x) => x ? "Yes" : "No";
     
     public static string ToYN(this bool x) => x ? "Y" : "N";
